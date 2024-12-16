3ad539804836030b34804669cb21466b462a8822
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:22:53 +0630

    -

--- MiniPOSSystemWithRepositoryDesignPattern.Shared/Extensions.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Shared/Extensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniPOSSystemWithRepositoryDesignPattern.Shared
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Shared
 {
     public static class Extensions
     {

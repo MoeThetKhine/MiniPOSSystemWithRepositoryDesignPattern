71d3eeb8f7983e7d47416014f1b5a5d8673e77f0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:26:30 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Utils/Enums/EnumStatusCode.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Utils/Enums/EnumStatusCode.cs
@@ -1,12 +1,11 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Utils.Enums
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Utils.Enums;
+
+public enum EnumStatusCode
 {
-    public enum EnumStatusCode
-    {
-        None,
-        Success = 200,
-        BadRequest = 400,
-        Conflict = 409,
-        NotFound = 404,
-        InternalServerError = 500
-    }
+    None,
+    Success = 200,
+    BadRequest = 400,
+    Conflict = 409,
+    NotFound = 404,
+    InternalServerError = 500
 }

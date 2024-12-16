be7c135e5187a3af198340f93ea71349291834d1
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 12:29:29 +0630

    Feat : Add MessageResouce

--- MiniPOSSystemWithRepositoryDesignPattern.Utils/Resources/MessageResource.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Utils/Resources/MessageResource.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace MiniPOSSystemWithRepositoryDesignPattern.Utils.Resources
 {
-    internal class MessageResource
+    public class MessageResource
     {
+        public static string Success { get; } = "Success";
+        public static string Fail { get; } = "Fail.";
     }
 }

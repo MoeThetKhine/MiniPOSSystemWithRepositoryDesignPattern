5243da7238cd9d95a05edfe3ae37e55061623660
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:40:41 +0630

    Feat : Add AdminModel

---
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Admin/AdminModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Admin
+{
+    public class AdminModel
+    {
+        public string UserId { get; set; } = null!;
+
+        public string UserName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public string? PhNo { get; set; }
+
+        public string UserRole { get; set; } = null!;
+
+        public bool? IsFirstTime { get; set; }
+
+        public bool? IsLocked { get; set; }
+
+        public bool? IsDelete { get; set; }
+    }
+}

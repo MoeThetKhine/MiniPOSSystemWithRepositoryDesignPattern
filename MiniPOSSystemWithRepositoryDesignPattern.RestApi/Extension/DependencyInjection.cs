5370e22e5bdd39fea1cabedec7ce49fc3d2c7361
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 20:30:41 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.RestApi/Extension/DependencyInjection.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.RestApi/Extension/DependencyInjection.cs
@@ -1,44 +1,43 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.RestApi.Extension
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.RestApi.Extension;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
     {
-
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            return services
-                .AddDbContextService(builder)
-                .AddDataAccessService()
-                .AddBusinessLogicService();
+        return services
+            .AddDbContextService(builder)
+            .AddDataAccessService()
+            .AddBusinessLogicService();
 
 
-        }
-        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Services
-             .AddDbContext<AppDbContext>(opt =>
-             {
-                 opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-             })
-             .AddControllers().AddJsonOptions(opt =>
-             {
-                 opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-             });
+    }
+    private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+    {
+        builder.Services
+         .AddDbContext<AppDbContext>(opt =>
+         {
+             opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+         })
+         .AddControllers().AddJsonOptions(opt =>
+         {
+             opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+         });
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddDataAccessService(this IServiceCollection services)
-        {
-           
-            return services
-            .AddScoped<IAdminRepository, AdminRepository>();
+    private static IServiceCollection AddDataAccessService(this IServiceCollection services)
+    {
+       
+        return services
+        .AddScoped<IAdminRepository, AdminRepository>();
 
-        }
+    }
 
-        private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
-        {
-            return services
-                .AddScoped<BL_Admin>();
-        }
+    private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<BL_Admin>();
     }
 }

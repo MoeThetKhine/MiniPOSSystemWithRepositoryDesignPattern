b6865d5dc4e47ecc13729c69fe57fd02ff9fa198
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:45:11 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Models/Product/ProductModel.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Product/ProductModel.cs
@@ -1,21 +1,20 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Product
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Product;
+
+public class ProductModel
 {
-    public class ProductModel
-    {
-        public string ProductId { get; set; } = null!;
+    public string ProductId { get; set; } = null!;
 
-        public string ProductCategoryId { get; set; } = null!;
+    public string ProductCategoryId { get; set; } = null!;
 
-        public string ProductName { get; set; } = null!;
+    public string ProductName { get; set; } = null!;
 
-        public string? Description { get; set; }
+    public string? Description { get; set; }
 
-        public int Qty { get; set; }
+    public int Qty { get; set; }
 
-        public decimal Price { get; set; }
+    public decimal Price { get; set; }
 
-        public DateTime? CreatedDate { get; set; }
+    public DateTime? CreatedDate { get; set; }
 
-        public bool? IsDelete { get; set; }
-    }
+    public bool? IsDelete { get; set; }
 }

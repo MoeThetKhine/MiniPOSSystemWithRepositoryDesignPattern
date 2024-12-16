3477a10cd48cca4953bfed48dc11261d952dc25a
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 12:31:37 +0630

    Feat : Add EnumStatusCode

--- MiniPOSSystemWithRepositoryDesignPattern.Utils/Enums/EnumStatusCode.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Utils/Enums/EnumStatusCode.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace MiniPOSSystemWithRepositoryDesignPattern.Utils.Enums
 {
-    internal class EnumStatusCode
+    public enum EnumStatusCode
     {
+        None,
+        Success = 200,
+        BadRequest = 400,
+        Conflict = 409,
+        NotFound = 404,
+        InternalServerError = 500
     }
 }

7e644531d92f6fa4a47aba302ca24d2c6a2f8917
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:44:04 +0630

    -

--- MiniPOSSystemWithRepositoryDesignPattern.Models/Sale/SaleModel.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Sale/SaleModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Sale
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Sale
 {
     public class SaleModel
     {

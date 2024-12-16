87ac2b59b8b2d240d39a3bec939d665921ccb758
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 15:50:34 +0630

    Feat : Add DependencyInjection

---
+++ MiniPOSSystemWithRepositoryDesignPattern.RestApi/Extension/DependencyInjection.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniPOSSystemWithRepositoryDesignPattern.Database.Models;
+using MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Admin;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.RestApi.Extension
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            return services
+                .AddDbContextService(builder)
+                .AddDataAccessService()
+                .AddBusinessLogicService();
+
+
+        }
+        private static IServiceCollection AddDbContextService(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            builder.Services
+             .AddDbContext<AppDbContext>(opt =>
+             {
+                 opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+             })
+             .AddControllers().AddJsonOptions(opt =>
+             {
+                 opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+             });
+
+            return services;
+        }
+
+        private static IServiceCollection AddDataAccessService(this IServiceCollection services)
+        {
+           
+            return services
+            .AddScoped<IAdminRepository, AdminRepository>();
+
+        }
+
+        private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<BL_Admin>();
+        }
+    }
+}

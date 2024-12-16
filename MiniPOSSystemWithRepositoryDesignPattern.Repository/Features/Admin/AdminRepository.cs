4c54163d65a2a37cb2212057cc6fd5be01179197
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 20:52:29 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/AdminRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/AdminRepository.cs
@@ -1,51 +1,50 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Admin
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Admin;
+
+public class AdminRepository : IAdminRepository
 {
-    public class AdminRepository : IAdminRepository
+    private readonly AppDbContext _db;
+
+    public AdminRepository(AppDbContext db)
     {
-        private readonly AppDbContext _db;
+        _db = db;
+    }
 
-        public AdminRepository(AppDbContext db)
+    public async Task<Result<List<AdminModel>>> GetAdminList(int pageNo, int pageSize, CancellationToken cs)
+    {
+        Result<List<AdminModel>> result;
+
+        try
         {
-            _db = db;
-        }
+            //var query = _context.TblBlogs
+            //.Where(x => x.IsActive == true)
+            //.Paginate(pageNo, pageSize);
 
-        public async Task<Result<List<AdminModel>>> GetAdminList(int pageNo, int pageSize, CancellationToken cs)
+            //var lst = await query.Select(x => new BlogModel()
+            //{
+            //    BlogId = x.BlogId,
+            //    BlogTitle = x.BlogTitle,
+            //    BlogAuthor = x.BlogAuthor,
+            //    BlogContent = x.BlogContent,
+            //    IsActive = true
+
+            //}).ToListAsync(cs);
+
+            //result = Result<List<BlogModel>>.Success(lst);
+
+            var query = _db.Admins
+                .Where(x => x.IsDelete == false)
+                .Paginate(pageNo, pageSize);
+
+            var lst = await query.Select(x => new AdminModel()
+            {
+
+            }).ToListAsync();
+
+        }
+        catch (Exception ex)
         {
-            Result<List<AdminModel>> result;
-
-            try
-            {
-                //var query = _context.TblBlogs
-                //.Where(x => x.IsActive == true)
-                //.Paginate(pageNo, pageSize);
-
-                //var lst = await query.Select(x => new BlogModel()
-                //{
-                //    BlogId = x.BlogId,
-                //    BlogTitle = x.BlogTitle,
-                //    BlogAuthor = x.BlogAuthor,
-                //    BlogContent = x.BlogContent,
-                //    IsActive = true
-
-                //}).ToListAsync(cs);
-
-                //result = Result<List<BlogModel>>.Success(lst);
-
-                var query = _db.Admins
-                    .Where(x => x.IsDelete == false)
-                    .Paginate(pageNo, pageSize);
-
-                var lst = await query.Select(x => new AdminModel()
-                {
-
-                }).ToListAsync();
-
-            }
-            catch (Exception ex)
-            {
-                result = Result<List<AdminModel>>.Fail(ex);
-            }
-            return result;
+            result = Result<List<AdminModel>>.Fail(ex);
         }
+        return result;
     }
 }

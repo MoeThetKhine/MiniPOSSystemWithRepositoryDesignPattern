397d1553e7f67a946a1534bb9ccbdb1b0210dd38
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 21:14:01 +0630

    -

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/ProductCategory/IProductCategoryRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/ProductCategory/IProductCategoryRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.ProductCategory
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.ProductCategory
 {
     internal interface IProductCategoryRepository
     {

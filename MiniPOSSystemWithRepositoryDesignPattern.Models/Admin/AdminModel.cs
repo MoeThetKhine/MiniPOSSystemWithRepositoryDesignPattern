54eee6403dd3f6f97862731f390546a977b8fa73
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:45:56 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Models/Admin/AdminModel.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Admin/AdminModel.cs
@@ -1,23 +1,22 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Admin
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Admin;
+
+public class AdminModel
 {
-    public class AdminModel
-    {
-        public string UserId { get; set; } = null!;
+    public string UserId { get; set; } = null!;
 
-        public string UserName { get; set; } = null!;
+    public string UserName { get; set; } = null!;
 
-        public string Email { get; set; } = null!;
+    public string Email { get; set; } = null!;
 
-        public string Password { get; set; } = null!;
+    public string Password { get; set; } = null!;
 
-        public string? PhNo { get; set; }
+    public string? PhNo { get; set; }
 
-        public string UserRole { get; set; } = null!;
+    public string UserRole { get; set; } = null!;
 
-        public bool? IsFirstTime { get; set; }
+    public bool? IsFirstTime { get; set; }
 
-        public bool? IsLocked { get; set; }
+    public bool? IsLocked { get; set; }
 
-        public bool? IsDelete { get; set; }
-    }
+    public bool? IsDelete { get; set; }
 }

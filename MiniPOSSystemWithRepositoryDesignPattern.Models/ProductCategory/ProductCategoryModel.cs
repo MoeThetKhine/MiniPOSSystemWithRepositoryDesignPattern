2618b1d1812557c72a003891f305534e03420c87
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:44:31 +0630

    -

--- MiniPOSSystemWithRepositoryDesignPattern.Models/ProductCategory/ProductCategoryModel.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/ProductCategory/ProductCategoryModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniPOSSystemWithRepositoryDesignPattern.Models.ProductCategory
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Models.ProductCategory
 {
     public class ProductCategoryModel
     {

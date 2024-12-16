e8fd9fb0dc77c90f7bf36569818f80ff421f624f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 20:36:39 +0630

    -

--- MiniPOSSystemWithRepositoryDesignPattern.Utils/Resources/MessageResource.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Utils/Resources/MessageResource.cs
@@ -1,7 +1,11 @@
 ﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Utils.Resources;
 
+#region MessageResource
+
 public class MessageResource
 {
     public static string Success { get; } = "Success";
     public static string Fail { get; } = "Fail.";
 }
+
+#endregion

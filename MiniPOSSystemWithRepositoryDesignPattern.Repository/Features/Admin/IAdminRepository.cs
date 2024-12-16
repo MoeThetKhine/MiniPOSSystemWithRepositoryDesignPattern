592a9a4e3c3d49b881db797352e7a8bfebe62a3f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 20:49:55 +0630

    -

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/IAdminRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/IAdminRepository.cs
@@ -1,10 +1,5 @@
 ﻿using MiniPOSSystemWithRepositoryDesignPattern.Models.Admin;
 using MiniPOSSystemWithRepositoryDesignPattern.Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Admin
 {

593026a39ebba122b25fe9567c6713bbb244e046
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:41:18 +0630

    Feat : Add ProductModel

---
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Product/ProductModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Product
+{
+    public class ProductModel
+    {
+        public string ProductId { get; set; } = null!;
+
+        public string ProductCategoryId { get; set; } = null!;
+
+        public string ProductName { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public int Qty { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+
+        public bool? IsDelete { get; set; }
+    }
+}

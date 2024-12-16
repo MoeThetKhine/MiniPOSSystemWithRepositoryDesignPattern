bf3556d3c6d8bf85fbcf05e554d4ed5560aec905
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:44:43 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Models/ProductCategory/ProductCategoryModel.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/ProductCategory/ProductCategoryModel.cs
@@ -1,11 +1,10 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Models.ProductCategory
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Models.ProductCategory;
+
+public class ProductCategoryModel
 {
-    public class ProductCategoryModel
-    {
-        public string ProductCategoryId { get; set; } = null!;
+    public string ProductCategoryId { get; set; } = null!;
 
-        public string ProductCategoryName { get; set; } = null!;
+    public string ProductCategoryName { get; set; } = null!;
 
-        public bool? IsDelete { get; set; }
-    }
+    public bool? IsDelete { get; set; }
 }

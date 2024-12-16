b142b52126ff1c5baac319c363234f77ad5e2f00
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:40:57 +0630

    Feat : Add Invoice Model

---
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Invoice/InvoiceModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Invoice
+{
+    public class InvoiceModel
+    {
+        public string InvoiceId { get; set; } = null!;
+
+        public decimal TotalAmount { get; set; }
+
+        public DateTime? InvoiceDate { get; set; }
+
+        public bool? IsDelete { get; set; }
+    }
+}

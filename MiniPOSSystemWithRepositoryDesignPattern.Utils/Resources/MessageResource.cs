17156f5ea1cda60fb45f007a1cf2561e8c4cc60e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:21:52 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Utils/Resources/MessageResource.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Utils/Resources/MessageResource.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniPOSSystemWithRepositoryDesignPattern.Utils.Resources
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Utils.Resources
 {
     public class MessageResource
     {

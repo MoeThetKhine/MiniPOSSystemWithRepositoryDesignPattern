6d54a1e36b367c83b008afceded391d27ea90c0c
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 21:09:04 +0630

    -

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Product/ProductRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Product/ProductRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Product
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Product
 {
     internal class ProductRepository
     {

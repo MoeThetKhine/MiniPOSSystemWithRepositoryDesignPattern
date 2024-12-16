ceff7f69995805ce4a71ba3b8fdebaf501aed965
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:27:05 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Utils/Result.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Utils/Result.cs
@@ -1,73 +1,72 @@
 ﻿using MiniPOSSystemWithRepositoryDesignPattern.Utils.Enums;
 
-namespace MiniPOSSystemWithRepositoryDesignPattern.Utils
+namespace MiniPOSSystemWithRepositoryDesignPattern.Utils;
+
+public class Result<T>
 {
-    public class Result<T>
+    public T Data { get; set; }
+    public EnumStatusCode StatusCode { get; set; }
+    public string Message { get; set; }
+    public bool IsSuccess { get; set; }
+
+    public static Result<T> Success(string message = "Success")
     {
-        public T Data { get; set; }
-        public EnumStatusCode StatusCode { get; set; }
-        public string Message { get; set; }
-        public bool IsSuccess { get; set; }
-
-        public static Result<T> Success(string message = "Success")
+        return new Result<T>
         {
-            return new Result<T>
-            {
-                Message = message,
-                IsSuccess = true,
-                StatusCode = EnumStatusCode.Success
-            };
-
-        }
-
-        public static Result<T> Success(T data, string message = "Success")
-        {
-            return new Result<T>
-            {
-                Data = data,
-                Message = message,
-                IsSuccess = true,
-                StatusCode = EnumStatusCode.Success
-            };
-        }
-        public static Result<T> Fail(string message = "Fail.", EnumStatusCode statusCode = EnumStatusCode.BadRequest)
-        {
-            return new Result<T>
-            {
-                Message = message,
-                IsSuccess = false,
-                StatusCode = statusCode
-            };
-        }
-
-        public static Result<T> Fail(Exception ex)
-        {
-            return new Result<T>
-            {
-                IsSuccess = false,
-                Message = ex.ToString(),
-                StatusCode = EnumStatusCode.InternalServerError
-            };
-        }
-        public static Result<T> Conflict(string message = "Data Conflict")
-        {
-            return new Result<T>
-            {
-                Message = message,
-                IsSuccess = false,
-                StatusCode = EnumStatusCode.Conflict
-            };
-        }
-
-        public static Result<T> NotFound(string message = "No Data Found")
-        {
-            return new Result<T>
-            {
-                Message = message,
-                IsSuccess = false,
-                StatusCode = EnumStatusCode.NotFound
-            };
-        }
+            Message = message,
+            IsSuccess = true,
+            StatusCode = EnumStatusCode.Success
+        };
 
     }
+
+    public static Result<T> Success(T data, string message = "Success")
+    {
+        return new Result<T>
+        {
+            Data = data,
+            Message = message,
+            IsSuccess = true,
+            StatusCode = EnumStatusCode.Success
+        };
+    }
+    public static Result<T> Fail(string message = "Fail.", EnumStatusCode statusCode = EnumStatusCode.BadRequest)
+    {
+        return new Result<T>
+        {
+            Message = message,
+            IsSuccess = false,
+            StatusCode = statusCode
+        };
+    }
+
+    public static Result<T> Fail(Exception ex)
+    {
+        return new Result<T>
+        {
+            IsSuccess = false,
+            Message = ex.ToString(),
+            StatusCode = EnumStatusCode.InternalServerError
+        };
+    }
+    public static Result<T> Conflict(string message = "Data Conflict")
+    {
+        return new Result<T>
+        {
+            Message = message,
+            IsSuccess = false,
+            StatusCode = EnumStatusCode.Conflict
+        };
+    }
+
+    public static Result<T> NotFound(string message = "No Data Found")
+    {
+        return new Result<T>
+        {
+            Message = message,
+            IsSuccess = false,
+            StatusCode = EnumStatusCode.NotFound
+        };
+    }
+
 }

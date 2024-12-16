598322a069492eea1fa28e9c5a477de71278da36
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:41:36 +0630

    Feat : Add ProductCategory Model

---
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/ProductCategory/ProductCategoryModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Models.ProductCategory
+{
+    public class ProductCategoryModel
+    {
+        public string ProductCategoryId { get; set; } = null!;
+
+        public string ProductCategoryName { get; set; } = null!;
+
+        public bool? IsDelete { get; set; }
+    }
+}

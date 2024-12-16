8d039842ff9dc298bb5f711317a48077685b8e56
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 20:51:10 +0630

    -

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/BL_Admin.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/BL_Admin.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Admin
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Admin
 {
     public class BL_Admin
     {

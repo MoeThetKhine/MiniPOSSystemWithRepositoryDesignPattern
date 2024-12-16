acba72a86b90a346bb11fe0cee4afac7a7a0e492
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:21:34 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Utils/Result.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Utils/Result.cs
@@ -1,9 +1,4 @@
 ﻿using MiniPOSSystemWithRepositoryDesignPattern.Utils.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiniPOSSystemWithRepositoryDesignPattern.Utils
 {

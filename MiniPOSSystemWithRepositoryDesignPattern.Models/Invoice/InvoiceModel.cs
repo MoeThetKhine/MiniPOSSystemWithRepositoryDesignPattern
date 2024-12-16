f2bb6fb18778ff3569c53188a7c8c2eeadef52ca
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:45:32 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Models/Invoice/InvoiceModel.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Invoice/InvoiceModel.cs
@@ -1,13 +1,12 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Invoice
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Invoice;
+
+public class InvoiceModel
 {
-    public class InvoiceModel
-    {
-        public string InvoiceId { get; set; } = null!;
+    public string InvoiceId { get; set; } = null!;
 
-        public decimal TotalAmount { get; set; }
+    public decimal TotalAmount { get; set; }
 
-        public DateTime? InvoiceDate { get; set; }
+    public DateTime? InvoiceDate { get; set; }
 
-        public bool? IsDelete { get; set; }
-    }
+    public bool? IsDelete { get; set; }
 }

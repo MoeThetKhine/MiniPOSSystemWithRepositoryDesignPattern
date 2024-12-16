b793a406df2532ac7713249f4826123baba5c5d0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-16 14:20:47 +0630

    Feat : Add Extension

--- MiniPOSSystemWithRepositoryDesignPattern.Shared/Extensions.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Shared/Extensions.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace MiniPOSSystemWithRepositoryDesignPattern.Shared
 {
-    internal class Extensions
+    public static class Extensions
     {
+        public static IQueryable<TSource> Paginate<TSource>(this IQueryable<TSource> sources, int pageNo, int pageSize)
+        {
+            return sources.Skip((pageNo - 1) * pageSize).Take(pageSize);
+        }
     }
 }

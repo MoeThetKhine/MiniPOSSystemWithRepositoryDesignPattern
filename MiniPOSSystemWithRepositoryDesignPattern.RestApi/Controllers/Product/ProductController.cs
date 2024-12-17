e150fa6717cd5fec60ecf28b3d24e3283f0918bc
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-17 21:26:06 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.RestApi/Controllers/Product/ProductController.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.RestApi/Controllers/Product/ProductController.cs
@@ -1,28 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Product;
 
-namespace MiniPOSSystemWithRepositoryDesignPattern.RestApi.Controllers.Product
+namespace MiniPOSSystemWithRepositoryDesignPattern.RestApi.Controllers.Product;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductController : ControllerBase
+    private readonly BL_Product _bL_Product;
+
+    public ProductController(BL_Product bL_Product)
     {
-        private readonly BL_Product _bL_Product;
-
-        public ProductController(BL_Product bL_Product)
-        {
-            _bL_Product = bL_Product;
-        }
-
-        #region GetProductAsync
-
-        [HttpGet]
-        public async Task<IActionResult> GetProductAsync(int pageNo, int pageSize, CancellationToken cs)
-        {
-            var lst = await _bL_Product.GetProductAsync(pageNo,pageSize,cs);
-            return Ok(lst);
-        }
-
-        #endregion
+        _bL_Product = bL_Product;
     }
+
+    #region GetProductAsync
+
+    [HttpGet]
+    public async Task<IActionResult> GetProductAsync(int pageNo, int pageSize, CancellationToken cs)
+    {
+        var lst = await _bL_Product.GetProductAsync(pageNo,pageSize,cs);
+        return Ok(lst);
+    }
+
+    #endregion
 }

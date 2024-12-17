e1f61a177268f7eb783df52cfd6468e0340cfa09
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-17 16:51:27 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.RestApi/Program.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.RestApi/Program.cs
@@ -2,10 +2,8 @@ using MiniPOSSystemWithRepositoryDesignPattern.RestApi.Extension;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,7 +11,6 @@ builder.Services.AddDependencyInjection(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();

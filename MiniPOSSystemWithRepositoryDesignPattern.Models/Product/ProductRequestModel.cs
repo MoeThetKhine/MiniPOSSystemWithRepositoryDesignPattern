501961d209e6790b1afcef03d5504f20b0b0ea36
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-17 21:19:17 +0630

    Feat : Add Product Request Model

---
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Product/ProductRequestModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Product
+{
+    public class ProductRequestModel
+    {
+        public string ProductId { get; set; } = null!;
+
+        public string ProductCategoryId { get; set; } = null!;
+
+        public string ProductName { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        public int Qty { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+    }
+}

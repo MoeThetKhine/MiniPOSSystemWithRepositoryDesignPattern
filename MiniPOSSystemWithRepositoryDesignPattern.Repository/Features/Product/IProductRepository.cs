7dfc007383374785747c67b18cf8518734315491
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-17 16:31:00 +0630

    Feat : Add GetProductAsync in IProductRepository

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Product/IProductRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Product/IProductRepository.cs
@@ -1,5 +1,9 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Product;
+﻿using MiniPOSSystemWithRepositoryDesignPattern.Models.Product;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Product;
 
 public interface IProductRepository
 {
+    Task<Result<IEnumerable<ProductModel>>> GetProductAsync(int pageNo, int pageSize, CancellationToken cs);
+
 }

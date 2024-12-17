e26c2510815748909181f48db1f5d4688b7e78f1
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-17 16:35:07 +0630

    Feat : Add GetProductAsync in BL_Product

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Product/BL_Product.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Product/BL_Product.cs
@@ -1,5 +1,30 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Product;
+﻿using MiniPOSSystemWithRepositoryDesignPattern.Models.Product;
+using System.Threading;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Product;
 
 public class BL_Product
 {
+    private readonly IProductRepository _productRepository;
+
+    public BL_Product(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<Result<IEnumerable<ProductModel>>> GetProductAsync(int pageNo, int pageSize, CancellationToken cs)
+    {
+        Result<IEnumerable<ProductModel>> response;
+        try
+        {
+            response = await _productRepository.GetProductAsync(pageNo,pageSize,cs);
+
+        }
+        catch (Exception ex)
+        {
+            response =  Result<IEnumerable<ProductModel>>.Fail(ex);
+        }
+    result:
+        return response;
+    }
 }

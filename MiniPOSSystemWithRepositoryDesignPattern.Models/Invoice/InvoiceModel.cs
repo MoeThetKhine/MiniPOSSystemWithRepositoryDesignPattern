ca17ce83eb18e4875cc9d91799b5e989dd28df77
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-28 22:37:11 +0630

    Feat : Add Invoice Model

--- MiniPOSSystemWithRepositoryDesignPattern.Models/Invoice/InvoiceModel.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Invoice/InvoiceModel.cs
@@ -6,9 +6,9 @@ public class InvoiceModel
 {
     public string InvoiceId { get; set; } = null!;
 
-    public decimal TotalAmount { get; set; }
-
     public DateTime? InvoiceDate { get; set; }
+
+    public decimal? TotalAmount { get; set; }
 
     public bool? IsDelete { get; set; }
 }

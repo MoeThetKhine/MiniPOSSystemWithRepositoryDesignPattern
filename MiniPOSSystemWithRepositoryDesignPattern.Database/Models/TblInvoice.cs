beed12c7abdf709170a4925cdfef01c08fea4c23
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-28 21:32:50 +0630

    -

--- MiniPOSSystemWithRepositoryDesignPattern.Database/Models/TblInvoice.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Database/Models/TblInvoice.cs
@@ -1,5 +1,7 @@
 ﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Database.Models;
 
+#region TblInvoice
+
 public partial class TblInvoice
 {
     public string InvoiceId { get; set; } = null!;
@@ -10,3 +12,5 @@ public partial class TblInvoice
 
     public bool? IsDelete { get; set; }
 }
+
+#endregion

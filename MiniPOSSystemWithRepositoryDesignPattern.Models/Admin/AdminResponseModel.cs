5f11c328cb50c5a8b5ec2baf657161fd595d9bd1
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-29 19:55:43 +0630

    Feat : Add AdminResponseModel

---
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Admin/AdminResponseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Admin
+{
+    public class AdminResponseModel
+    {
+        public string? UserName { get; set; }
+
+        public string? Password { get; set; }
+
+        public string PhNo { get; set; }
+    }
+}

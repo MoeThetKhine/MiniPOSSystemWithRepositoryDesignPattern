2103b31851dca724c03972ab6e6ba7a3636ea181
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-29 12:23:18 +0630

    Feat : Add GetAdminAsync in AdminController

--- MiniPOSSystemWithRepositoryDesignPattern.RestApi/Controllers/Admin/AdminController.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.RestApi/Controllers/Admin/AdminController.cs
@@ -7,5 +7,18 @@ namespace MiniPOSSystemWithRepositoryDesignPattern.RestApi.Controllers.Admin
     [ApiController]
     public class AdminController : ControllerBase
     {
+        private readonly BL_Admin bL_Admin;
+
+        public AdminController(BL_Admin bL_Admin)
+        {
+            this.bL_Admin = bL_Admin;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult>GetAdminAsync(int pageSize, int pageNo , CancellationToken cs)
+        {
+            var item = await bL_Admin.GetAdminAsync(pageSize, pageNo, cs);
+            return Ok(item);
+        }
     }
 }

8fb61b08ea54679c75d745e91c81e5a9bfb47397
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-29 13:09:15 +0630

    Feat : Add CreateAdminAsync in AdminRepository

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/AdminRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/AdminRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Admin;
+﻿using System.Threading;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Admin;
 
 public class AdminRepository : IAdminRepository
 {
@@ -9,6 +11,7 @@ public class AdminRepository : IAdminRepository
         _db = db;
     }
 
+    
     #region GetAdminAsync
 
     public async Task<Result<IEnumerable<AdminModel>>> GetAdminAsync(int pageNo, int pageSize, CancellationToken cs)
@@ -40,4 +43,45 @@ public class AdminRepository : IAdminRepository
     }
 
     #endregion
+
+    public async Task<Result<AdminRequestModel>> CreateAdminAsync(AdminRequestModel adminRequest, CancellationToken cs)
+    {
+        Result<AdminRequestModel> result;
+        try
+        {
+            var existingAdmin = _db.TblAdmins.AsNoTracking().FirstOrDefaultAsync(x => x.Email == adminRequest.Email && !x.IsDelete && x.PhNo == adminRequest.PhNo);
+
+            if(existingAdmin is not null)
+            {
+                result = Result<AdminRequestModel>.Conflict("Admin already exist.");
+            }
+            string adminId = Ulid.NewUlid().ToString();
+
+            var item = new TblAdmin()
+            {
+                UserId = adminId,
+                UserName = adminRequest.UserName,
+                Password = adminRequest.Password,
+                Email = adminRequest.Email,
+                PhNo = adminRequest.PhNo,
+                UserRole = "Admin",
+                IsDelete = false,
+                IsFirstTime = true,
+                IsLocked = false
+            };
+
+            await _db.TblAdmins.AddAsync(item, cs);
+            await _db.SaveChangesAsync(cs);
+
+            result = Result<AdminRequestModel>.Success();
+        }
+        catch(Exception ex)
+        {
+            result = Result<AdminRequestModel>.Fail(ex);
+        }
+        return result;
+    }
+
+
+
 }

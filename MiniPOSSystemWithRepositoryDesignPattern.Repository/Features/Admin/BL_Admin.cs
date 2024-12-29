4795eb37ed6d1a08f01f67dd9ae47e562d3c8b0f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-29 12:23:06 +0630

    Feat : Add GetAdminAsync in BL_Admin

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/BL_Admin.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/BL_Admin.cs
@@ -2,4 +2,24 @@
 
 public class BL_Admin
 {
+    private readonly IAdminRepository _adminRepository;
+
+    public BL_Admin(IAdminRepository adminRepository)
+    {
+        _adminRepository = adminRepository;
+    }
+    public async Task<Result<IEnumerable<AdminModel>>> GetAdminAsync(int pageNo, int pageSize, CancellationToken cs)
+    {
+        Result<IEnumerable<AdminModel>> result;
+
+        try
+        {
+            result = await _adminRepository.GetAdminAsync(pageNo, pageSize, cs);
+        }
+        catch (Exception ex)
+        {
+            result = Result<IEnumerable<AdminModel>>.Fail(ex);
+        }
+        return result;
+    }
 }

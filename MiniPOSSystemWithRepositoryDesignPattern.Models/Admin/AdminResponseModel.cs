71cf52110ccde97860c7f2a1f4f8bc45ec119c08
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-29 19:56:18 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Models/Admin/AdminResponseModel.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Admin/AdminResponseModel.cs
@@ -1,11 +1,10 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Admin
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Admin;
+
+public class AdminResponseModel
 {
-    public class AdminResponseModel
-    {
-        public string? UserName { get; set; }
+    public string? UserName { get; set; }
 
-        public string? Password { get; set; }
+    public string? Password { get; set; }
 
-        public string PhNo { get; set; }
-    }
+    public string PhNo { get; set; }
 }

af6bbd75564a87fc77faa121288d71027da605ce
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-29 12:25:20 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.RestApi/Controllers/Admin/AdminController.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.RestApi/Controllers/Admin/AdminController.cs
@@ -1,26 +1,25 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.RestApi.Controllers.Admin
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.RestApi.Controllers.Admin;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AdminController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AdminController : ControllerBase
+    private readonly BL_Admin bL_Admin;
+
+    public AdminController(BL_Admin bL_Admin)
     {
-        private readonly BL_Admin bL_Admin;
-
-        public AdminController(BL_Admin bL_Admin)
-        {
-            this.bL_Admin = bL_Admin;
-        }
-
-        #region GetAdminAsync
-
-        [HttpGet]
-        public async Task<IActionResult>GetAdminAsync(int pageSize, int pageNo , CancellationToken cs)
-        {
-            var item = await bL_Admin.GetAdminAsync(pageSize, pageNo, cs);
-            return Ok(item);
-        }
-
-        #endregion
-
+        this.bL_Admin = bL_Admin;
     }
+
+    #region GetAdminAsync
+
+    [HttpGet]
+    public async Task<IActionResult>GetAdminAsync(int pageSize, int pageNo , CancellationToken cs)
+    {
+        var item = await bL_Admin.GetAdminAsync(pageSize, pageNo, cs);
+        return Ok(item);
+    }
+
+    #endregion
+
 }

ff63749222238b8564f7b8c9871a5f4bfe23c61f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-29 11:07:03 +0630

    Feat : Add CreateProductAsync in BL_Product

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Product/BL_Product.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Product/BL_Product.cs
@@ -9,6 +9,38 @@ public class BL_Product
         _productRepository = productRepository;
     }
 
-   
+    public async Task<Result<ProductRequestModel>> CreateProductAsync(ProductRequestModel productRequestModel, CancellationToken cancellationToken)
+    {
+        Result<ProductRequestModel> result;
+        try
+        {
+            if(productRequestModel.ProductName is null)
+            {
+                result = Result<ProductRequestModel>.Fail("Product Name Is Required.");
+            }
+            if (productRequestModel.Description is null)
+            {
+                result = Result<ProductRequestModel>.Fail("Description Is Required.");
+            }
+            if (productRequestModel.Price is null)
+            {
+                result = Result<ProductRequestModel>.Fail("Price Is Required.");
+            }
+            if (productRequestModel.ProductCategoryId is null)
+            {
+                result = Result<ProductRequestModel>.Fail("ProductCategoryId Is Required.");
+            }
+
+            result = await _productRepository.CreateProductAsync(productRequestModel, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            result = Result<ProductRequestModel>.Fail(ex);
+        }
+        return result;
+    }
+
+
+
 
 }

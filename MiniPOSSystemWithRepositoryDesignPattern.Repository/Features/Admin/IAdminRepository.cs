fa69d9071cd4c4f4ccb80a8ca17b5aae192879bc
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-30 11:39:26 +0630

    Feat : Add ActivateAdminAsync IAdminRepository

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/IAdminRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/IAdminRepository.cs
@@ -7,5 +7,8 @@ public interface IAdminRepository
     Task<Result<AdminRequestModel>> CreateAdminAsync(AdminRequestModel adminRequest, CancellationToken cs);
     Task<Result<AdminResponseModel>> UpdateAdminAsync(string name, AdminResponseModel adminResponse,CancellationToken cs);
     Task<Result<AdminModel>> DeleteAdminAsync(string name, CancellationToken cs);
+    Task<Result<AdminModel>> ActivateAdminAsync(string name, CancellationToken cs);
+
+
 
 }

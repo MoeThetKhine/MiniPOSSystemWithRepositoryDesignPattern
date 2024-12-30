99308efa3590d4eede7341923f219ead466c545e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-30 22:20:41 +0630

    Feat : Add UpdateProductAsync in IProductRepository

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Product/IProductRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Product/IProductRepository.cs
@@ -3,6 +3,10 @@
 public interface IProductRepository
 {
     Task<Result<IEnumerable<ProductModel>>> GetProductAsync (CancellationToken cs);
-    Task<Result<IEnumerable<ProductModel>>> GetProductByCategoryIdAsync(string CategoryId , CancellationToken cs);
-    Task<Result<ProductRequestModel>> CreateProductAsync(ProductRequestModel productRequestModel, CancellationToken cancellationToken);
+    Task<Result<IEnumerable<ProductModel>>> GetProductByCategoryIdAsync
+        (string CategoryId , CancellationToken cs);
+    Task<Result<ProductRequestModel>> CreateProductAsync
+        (ProductRequestModel productRequestModel, CancellationToken cancellationToken);
+    Task<Result<ProductResponseModel>> UpdateProductAsync
+        (string productId, ProductResponseModel productResponse, CancellationToken cs);
 }

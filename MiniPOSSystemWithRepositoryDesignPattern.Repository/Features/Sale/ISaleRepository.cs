c6403eedc8b1f6bdbd88d3db6690c629ceb98856
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 17:25:45 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/ISaleRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/ISaleRepository.cs
@@ -1,7 +1,6 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Sale
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Sale;
+
+public interface ISaleRepository
 {
-    public interface ISaleRepository
-    {
-        Task<Result<IEnumerable<SaleModel>>> GetSaleListAsync(int pageSize , int pageNo , CancellationToken cs);
-    }
+    Task<Result<IEnumerable<SaleModel>>> GetSaleListAsync(int pageSize , int pageNo , CancellationToken cs);
 }

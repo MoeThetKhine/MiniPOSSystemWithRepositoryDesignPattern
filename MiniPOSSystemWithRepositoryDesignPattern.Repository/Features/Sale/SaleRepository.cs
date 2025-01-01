8a8c87781378d6583a4d87d685da54df65372057
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 17:24:54 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/SaleRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/SaleRepository.cs
@@ -1,38 +1,37 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Sale
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Sale;
+
+public class SaleRepository : ISaleRepository
 {
-    public class SaleRepository : ISaleRepository
+    private readonly AppDbContext _appDbContext;
+
+    public SaleRepository(AppDbContext appDbContext)
     {
-        private readonly AppDbContext _appDbContext;
+        _appDbContext = appDbContext;
+    }
 
-        public SaleRepository(AppDbContext appDbContext)
+    public async Task<Result<IEnumerable<SaleModel>>> GetSaleListAsync(int pageSize, int pageNo, CancellationToken cs)
+    {
+        Result<IEnumerable<SaleModel>> result;
+
+        try
         {
-            _appDbContext = appDbContext;
-        }
+            var sale = _appDbContext.TblSales.AsNoTracking();
 
-        public async Task<Result<IEnumerable<SaleModel>>> GetSaleListAsync(int pageSize, int pageNo, CancellationToken cs)
+            var lst = await sale.Select(x => new SaleModel()
+            {
+                SaleId = x.SaleId,
+                ProductId = x.ProductId,
+                InvoiceId = x.InvoiceId,
+                UnitPrice = x.UnitPrice,
+                CreateDate = x.CreateDate,
+            }).ToListAsync();
+
+            result = Result<IEnumerable<SaleModel>>.Success(lst);
+        }
+        catch (Exception ex)
         {
-            Result<IEnumerable<SaleModel>> result;
-
-            try
-            {
-                var sale = _appDbContext.TblSales.AsNoTracking();
-
-                var lst = await sale.Select(x => new SaleModel()
-                {
-                    SaleId = x.SaleId,
-                    ProductId = x.ProductId,
-                    InvoiceId = x.InvoiceId,
-                    UnitPrice = x.UnitPrice,
-                    CreateDate = x.CreateDate,
-                }).ToListAsync();
-
-                result = Result<IEnumerable<SaleModel>>.Success(lst);
-            }
-            catch (Exception ex)
-            {
-                result =  Result<IEnumerable<SaleModel>>.Fail(ex);
-            }
-            return result;
+            result =  Result<IEnumerable<SaleModel>>.Fail(ex);
         }
+        return result;
     }
 }

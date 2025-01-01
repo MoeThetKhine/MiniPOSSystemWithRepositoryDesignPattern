f37e66c6cf1daa1b7b29ff6100dd9adae3dbcb65
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 17:21:32 +0630

    Feat : Add GetSaleListAsync in SaleRepository

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/SaleRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/SaleRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiniPOSSystemWithRepositoryDesignPattern.Models.Sale;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,37 @@ namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Sale
 {
     public class SaleRepository : ISaleRepository
     {
+        private readonly AppDbContext _appDbContext;
+
+        public SaleRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<Result<IEnumerable<SaleModel>>> GetSaleListAsync(int pageSize, int pageNo, CancellationToken cs)
+        {
+            Result<IEnumerable<SaleModel>> result;
+
+            try
+            {
+                var sale = _appDbContext.TblSales.AsNoTracking();
+
+                var lst = await sale.Select(x => new SaleModel()
+                {
+                    SaleId = x.SaleId,
+                    ProductId = x.ProductId,
+                    InvoiceId = x.InvoiceId,
+                    UnitPrice = x.UnitPrice,
+                    CreateDate = x.CreateDate,
+                }).ToListAsync();
+
+                result = Result<IEnumerable<SaleModel>>.Success(lst);
+            }
+            catch (Exception ex)
+            {
+                result =  Result<IEnumerable<SaleModel>>.Fail(ex);
+            }
+            return result;
+        }
     }
 }

4142d474d1e292c115ceadfb92c3f9f8917918dd
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 21:37:22 +0630

    Feat : Add GetInvoiceListAsync in BL_Invoice

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Invoice/BL_Invoice.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Invoice/BL_Invoice.cs
@@ -1,6 +1,29 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Invoice
+﻿using MiniPOSSystemWithRepositoryDesignPattern.Models.Invoice;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Invoice
 {
     public class BL_Invoice
     {
+        private readonly IInvoiceRepository _invoiceRepository;
+
+        public BL_Invoice(IInvoiceRepository invoiceRepository)
+        {
+            _invoiceRepository = invoiceRepository;
+        }
+
+        public async Task<Result<IEnumerable<InvoiceModel>>> GetInvoiceListAsync(int pageNo, int pageSize, CancellationToken cs)
+        {
+            Result<IEnumerable<InvoiceModel>> result;
+
+            try
+            {
+                result = await _invoiceRepository.GetInvoiceListAsync(pageNo, pageSize, cs);
+            }
+            catch (Exception ex)
+            {
+                result = Result<IEnumerable<InvoiceModel>>.Fail(ex);
+            }
+            return result;
+        }
     }
 }

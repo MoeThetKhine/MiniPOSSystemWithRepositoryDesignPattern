21233a8fb94786f43c42465f442d01bfdaa3ef60
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 21:25:34 +0630

    Feat : Add GetInvoiceListAsync in IInvoiceRepository

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Invoice/IInvoiceRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Invoice/IInvoiceRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Invoice
+﻿using MiniPOSSystemWithRepositoryDesignPattern.Models.Invoice;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Invoice
 {
     public interface IInvoiceRepository
     {
+        Task<Result<IEnumerable<InvoiceModel>>> GetInvoiceListAsync(int pageNo, int pageSize, CancellationToken cs);
     }
 }
--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Invoice/InvoiceRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Invoice/InvoiceRepository.cs
@@ -1,6 +1,39 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Invoice
+﻿using MiniPOSSystemWithRepositoryDesignPattern.Models.Invoice;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Invoice
 {
     public class InvoiceRepository : IInvoiceRepository
     {
+        private readonly AppDbContext _db;
+
+        public InvoiceRepository(AppDbContext appDbContext)
+        {
+            _db = appDbContext;
+        }
+
+        public async Task<Result<IEnumerable<InvoiceModel>>> GetInvoiceListAsync(int pageNo, int pageSize, CancellationToken cs)
+        {
+            Result<IEnumerable<InvoiceModel>> result;
+
+            try
+            {
+                var invoice = _db.TblInvoices.AsNoTracking();
+
+                var lst = await invoice.Select(x => new InvoiceModel()
+                {
+                    InvoiceId = x.InvoiceId,
+                    InvoiceDate = x.InvoiceDate,
+                    TotalAmount = x.TotalAmount,
+                    IsDelete = x.IsDelete
+                }).ToListAsync();
+
+                result = Res
+            }
+            catch (Exception ex)
+            {
+                result = Result<IEnumerable<InvoiceModel>>.Fail(ex);
+            }
+            return result;
+        }
     }
 }

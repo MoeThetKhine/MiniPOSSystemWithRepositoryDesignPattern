b117f677fc9b9efefbe8a15b807b41489b193fb0
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 22:24:28 +0630

    Feat : Add CreateSaleAsync in ISaleRepository

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/ISaleRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/ISaleRepository.cs
@@ -5,6 +5,8 @@
 public interface ISaleRepository
 {
     Task<Result<IEnumerable<SaleModel>>> GetSaleListAsync(int pageSize , int pageNo , CancellationToken cs);
+
+    Task<Result<SaleRequestModel>> CreateSaleAsync(SaleRequestModel saleRequest, CancellationToken cancellationToken);
 }
 
 #endregion

a03bbc87993797c854e158612f2dff11d613174a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 21:51:18 +0630

    Feat : Add SaleRequestModel

---
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Sale/SaleRequestModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Sale
+{
+    public class SaleRequestModel
+    {
+        public string? ProductId { get; set; }
+
+        public string? InvoiceId { get; set; }
+
+        public decimal? UnitPrice { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+    }
+}

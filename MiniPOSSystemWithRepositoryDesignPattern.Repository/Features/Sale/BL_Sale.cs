1527ae9ef55666d65c0417ef03e62356197b525b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 17:21:46 +0630

    Feat : Add GetSaleListAsync in BL_Sale

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/BL_Sale.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/BL_Sale.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiniPOSSystemWithRepositoryDesignPattern.Models.Sale;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,26 @@ namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Sale
 {
     public class BL_Sale
     {
+        private readonly ISaleRepository _saleRepository;
+
+        public BL_Sale(ISaleRepository saleRepository)
+        {
+            _saleRepository = saleRepository;
+        }
+
+        public async Task<Result<IEnumerable<SaleModel>>> GetSaleListAsync(int pageSize, int pageNo, CancellationToken cs)
+        {
+            Result<IEnumerable<SaleModel>> result;
+
+            try
+            {
+                result = await _saleRepository.GetSaleListAsync(pageSize, pageNo, cs);
+            }
+            catch (Exception ex)
+            {
+                result = Result<IEnumerable<SaleModel>>.Fail(ex);
+            }
+            return result;
+        }
     }
 }

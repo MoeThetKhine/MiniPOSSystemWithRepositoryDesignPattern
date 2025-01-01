6534bd0877d2ea6a577a64590901f050dbafb72f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 21:47:24 +0630

    -

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Invoice/IInvoiceRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Invoice/IInvoiceRepository.cs
@@ -1,6 +1,10 @@
 ﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Invoice;
 
+#region IInvoiceRepository
+
 public interface IInvoiceRepository
 {
     Task<Result<IEnumerable<InvoiceModel>>> GetInvoiceListAsync(int pageNo, int pageSize, CancellationToken cs);
 }
+
+#endregion

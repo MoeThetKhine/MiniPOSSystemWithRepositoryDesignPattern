36bc138f28bd347135a8e31a313e86771c0628e2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 17:21:13 +0630

    Feat : Add GetSaleListAsync in ISaleRepository

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/ISaleRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/ISaleRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiniPOSSystemWithRepositoryDesignPattern.Models.Sale;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Sale
 {
     public interface ISaleRepository
     {
+        Task<Result<IEnumerable<SaleModel>>> GetSaleListAsync(int pageSize , int pageNo , CancellationToken cs);
     }
 }

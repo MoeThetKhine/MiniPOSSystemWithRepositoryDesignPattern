bdcdb29e79dce1192b1596a328a3199ec84362c2
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 17:27:02 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.RestApi/Controllers/Sale/SaleController.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.RestApi/Controllers/Sale/SaleController.cs
@@ -1,23 +1,22 @@
 ﻿using MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Sale;
 
-namespace MiniPOSSystemWithRepositoryDesignPattern.RestApi.Controllers.Sale
+namespace MiniPOSSystemWithRepositoryDesignPattern.RestApi.Controllers.Sale;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SaleController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class SaleController : ControllerBase
+    private readonly BL_Sale _bL_Sale;
+
+    public SaleController(BL_Sale bL_Sale)
     {
-        private readonly BL_Sale _bL_Sale;
+        _bL_Sale = bL_Sale;
+    }
 
-        public SaleController(BL_Sale bL_Sale)
-        {
-            _bL_Sale = bL_Sale;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetSaleListAsync(int pageSize, int pageNo, CancellationToken cs)
-        {
-            var result = await _bL_Sale.GetSaleListAsync(pageSize, pageNo, cs); 
-            return Ok(result);
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetSaleListAsync(int pageSize, int pageNo, CancellationToken cs)
+    {
+        var result = await _bL_Sale.GetSaleListAsync(pageSize, pageNo, cs); 
+        return Ok(result);
     }
 }

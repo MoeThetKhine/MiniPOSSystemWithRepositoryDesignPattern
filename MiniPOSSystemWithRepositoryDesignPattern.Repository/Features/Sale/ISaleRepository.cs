18c5e72f673b699b33f341741b3338060aa291a5
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 17:26:05 +0630

    -

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/ISaleRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/ISaleRepository.cs
@@ -1,6 +1,10 @@
 ﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Sale;
 
+#region ISaleRepository
+
 public interface ISaleRepository
 {
     Task<Result<IEnumerable<SaleModel>>> GetSaleListAsync(int pageSize , int pageNo , CancellationToken cs);
 }
+
+#endregion

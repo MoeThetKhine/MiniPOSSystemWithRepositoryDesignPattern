5a6a948f85c41efa2282edf8bbbf294c6774855d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 17:26:26 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/BL_Sale.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/BL_Sale.cs
@@ -1,27 +1,26 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Sale
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Sale;
+
+public class BL_Sale
 {
-    public class BL_Sale
+    private readonly ISaleRepository _saleRepository;
+
+    public BL_Sale(ISaleRepository saleRepository)
     {
-        private readonly ISaleRepository _saleRepository;
+        _saleRepository = saleRepository;
+    }
 
-        public BL_Sale(ISaleRepository saleRepository)
+    public async Task<Result<IEnumerable<SaleModel>>> GetSaleListAsync(int pageSize, int pageNo, CancellationToken cs)
+    {
+        Result<IEnumerable<SaleModel>> result;
+
+        try
         {
-            _saleRepository = saleRepository;
+            result = await _saleRepository.GetSaleListAsync(pageSize, pageNo, cs);
         }
-
-        public async Task<Result<IEnumerable<SaleModel>>> GetSaleListAsync(int pageSize, int pageNo, CancellationToken cs)
+        catch (Exception ex)
         {
-            Result<IEnumerable<SaleModel>> result;
-
-            try
-            {
-                result = await _saleRepository.GetSaleListAsync(pageSize, pageNo, cs);
-            }
-            catch (Exception ex)
-            {
-                result = Result<IEnumerable<SaleModel>>.Fail(ex);
-            }
-            return result;
+            result = Result<IEnumerable<SaleModel>>.Fail(ex);
         }
+        return result;
     }
 }

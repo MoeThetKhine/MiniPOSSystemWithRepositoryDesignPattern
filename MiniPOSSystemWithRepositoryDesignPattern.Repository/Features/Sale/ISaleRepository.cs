c20a56da9acbaa9cd7e72e1e9c20863fc884d0fd
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 17:23:43 +0630

    -

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/ISaleRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Sale/ISaleRepository.cs
@@ -1,9 +1,4 @@
 ﻿using MiniPOSSystemWithRepositoryDesignPattern.Models.Sale;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Sale
 {

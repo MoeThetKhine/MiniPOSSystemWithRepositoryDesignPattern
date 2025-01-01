d0765c8c2fc61a47e6a1b6657f9a8673a45b8ce5
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 21:46:30 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Invoice/InvoiceRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Invoice/InvoiceRepository.cs
@@ -1,42 +1,41 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Invoice
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.Repository.Features.Invoice;
+
+public class InvoiceRepository : IInvoiceRepository
 {
-    public class InvoiceRepository : IInvoiceRepository
+    private readonly AppDbContext _db;
+
+    public InvoiceRepository(AppDbContext appDbContext)
     {
-        private readonly AppDbContext _db;
-
-        public InvoiceRepository(AppDbContext appDbContext)
-        {
-            _db = appDbContext;
-        }
-
-        #region GetInvoiceListAsync
-
-        public async Task<Result<IEnumerable<InvoiceModel>>> GetInvoiceListAsync(int pageNo, int pageSize, CancellationToken cs)
-        {
-            Result<IEnumerable<InvoiceModel>> result;
-
-            try
-            {
-                var invoice = _db.TblInvoices.AsNoTracking();
-
-                var lst = await invoice.Select(x => new InvoiceModel()
-                {
-                    InvoiceId = x.InvoiceId,
-                    InvoiceDate = x.InvoiceDate,
-                    TotalAmount = x.TotalAmount,
-                    IsDelete = x.IsDelete
-                }).ToListAsync();
-
-                result = Result<IEnumerable<InvoiceModel>>.Success(lst);
-            }
-            catch (Exception ex)
-            {
-                result = Result<IEnumerable<InvoiceModel>>.Fail(ex);
-            }
-            return result;
-        }
-
-        #endregion
-
+        _db = appDbContext;
     }
+
+    #region GetInvoiceListAsync
+
+    public async Task<Result<IEnumerable<InvoiceModel>>> GetInvoiceListAsync(int pageNo, int pageSize, CancellationToken cs)
+    {
+        Result<IEnumerable<InvoiceModel>> result;
+
+        try
+        {
+            var invoice = _db.TblInvoices.AsNoTracking();
+
+            var lst = await invoice.Select(x => new InvoiceModel()
+            {
+                InvoiceId = x.InvoiceId,
+                InvoiceDate = x.InvoiceDate,
+                TotalAmount = x.TotalAmount,
+                IsDelete = x.IsDelete
+            }).ToListAsync();
+
+            result = Result<IEnumerable<InvoiceModel>>.Success(lst);
+        }
+        catch (Exception ex)
+        {
+            result = Result<IEnumerable<InvoiceModel>>.Fail(ex);
+        }
+        return result;
+    }
+
+    #endregion
+
 }

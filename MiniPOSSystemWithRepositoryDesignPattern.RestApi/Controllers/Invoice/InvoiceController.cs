d8f8cf57f94c8b2b0dfa8f8b655874e9dd173730
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-01 21:44:00 +0630

    Format Code

--- MiniPOSSystemWithRepositoryDesignPattern.RestApi/Controllers/Invoice/InvoiceController.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.RestApi/Controllers/Invoice/InvoiceController.cs
@@ -1,26 +1,25 @@
-﻿namespace MiniPOSSystemWithRepositoryDesignPattern.RestApi.Controllers.Invoice
+﻿namespace MiniPOSSystemWithRepositoryDesignPattern.RestApi.Controllers.Invoice;
+
+[Route("api/[controller]")]
+[ApiController]
+public class InvoiceController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class InvoiceController : ControllerBase
+    private readonly BL_Invoice _bL_Invoice;
+
+    public InvoiceController(BL_Invoice bL_Invoice)
     {
-        private readonly BL_Invoice _bL_Invoice;
-
-        public InvoiceController(BL_Invoice bL_Invoice)
-        {
-            _bL_Invoice = bL_Invoice;
-        }
-
-        #region GetInvoiceListAsync
-
-        [HttpGet]
-        public async Task<IActionResult> GetInvoiceListAsync(int pageNo, int pageSize, CancellationToken cs)
-        {
-            var result = await _bL_Invoice.GetInvoiceListAsync(pageNo, pageSize, cs);
-            return Ok(result);
-        }
-
-        #endregion
-
+        _bL_Invoice = bL_Invoice;
     }
+
+    #region GetInvoiceListAsync
+
+    [HttpGet]
+    public async Task<IActionResult> GetInvoiceListAsync(int pageNo, int pageSize, CancellationToken cs)
+    {
+        var result = await _bL_Invoice.GetInvoiceListAsync(pageNo, pageSize, cs);
+        return Ok(result);
+    }
+
+    #endregion
+
 }

0e0c0d3c9cf8518435788f7ef5799a14afeeb789
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-02 12:27:41 +0630

    Feat : Add InvoiceRequestModel

---
+++ MiniPOSSystemWithRepositoryDesignPattern.Models/Invoice/InvoiceRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniPOSSystemWithRepositoryDesignPattern.Models.Invoice
+{
+    public class InvoiceRequestModel
+    {
+        public DateTime? InvoiceDate { get; set; }
+
+        public decimal? TotalAmount { get; set; }
+
+    }
+}

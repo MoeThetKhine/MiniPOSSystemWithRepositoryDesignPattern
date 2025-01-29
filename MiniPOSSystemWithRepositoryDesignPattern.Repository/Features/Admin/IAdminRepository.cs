b303324aef3bcab950b41c84894fc3c9d9bab038
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-29 15:50:29 +0630

    Feat : Update IAdminRepository

--- MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/IAdminRepository.cs
+++ MiniPOSSystemWithRepositoryDesignPattern.Repository/Features/Admin/IAdminRepository.cs
@@ -3,7 +3,6 @@
 public interface IAdminRepository
 {
     Task<Result<IEnumerable<AdminModel>>> GetAdminAsync(int pageNo, int pageSize, CancellationToken cs);
-
     Task<Result<AdminRequestModel>> CreateAdminAsync(AdminRequestModel adminRequest, CancellationToken cs);
     Task<Result<AdminResponseModel>> UpdateAdminAsync(string name, AdminResponseModel adminResponse,CancellationToken cs);
     Task<Result<AdminModel>> DeleteAdminAsync(string name, CancellationToken cs);
